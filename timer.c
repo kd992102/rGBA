1e35b34db85b16000efa6980a046d1e35c9dbbf5
kd992102 <kd992102@gmail.com>
2024-03-01 13:46:20 +0800

    2024.03.01

--- timer.c
+++ timer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "io.h"
 #include "timer.h"
 #include "sound.h"
@@ -9,54 +10,39 @@ void Timer_Clock(uint32_t cycle){
     uint8_t overflow = 0;
     uint16_t count, reload;
     uint16_t TM_CNT[4];
+    uint16_t TM_COUNT[4];
     uint32_t tmr_increcnt[4];
     TM_CNT[0] = MemRead16(TM0CNT);
     TM_CNT[1] = MemRead16(TM1CNT);
     TM_CNT[2] = MemRead16(TM2CNT);
     TM_CNT[3] = MemRead16(TM3CNT);
+
     for(idx = 0; idx < 4; idx++){
         //If TM_REG Not enable, continue
         if(!(TM_CNT[idx] & TMR_ENB)){
             overflow = 0;
             continue;
         }
+        if(idx != 0)printf("i:%d, cnt:%d\n", idx, TM_CNT[idx]);
 
-        if(TM_CNT[idx] & TMR_CASCADE){
+        if(TM_CNT[idx] & TMR_CASCADE && idx != 0){
+            printf("timers cascade\n");
             if(overflow){
-                switch(idx){
-                    case 0:
-                        count = MemRead16(TM0_D);
-                        MemWrite16(TM0_D, count+1);
-                        count = MemRead16(TM0_D);
-                        break;
-                    case 1:
-                        count = MemRead16(TM1_D);
-                        MemWrite16(TM1_D, count+1);
-                        count = MemRead16(TM1_D);
-                        break;
-                    case 2:
-                        count = MemRead16(TM2_D);
-                        MemWrite16(TM2_D, count+1);
-                        count = MemRead16(TM2_D);
-                        break;
-                    case 3:
-                        count = MemRead16(TM3_D);
-                        MemWrite16(TM3_D, count+1);
-                        count = MemRead16(TM3_D);
-                        break;
-                }
-            }
-            else{
-                uint8_t shift = shift_lut[TM_CNT[idx] & 3];
-                uint64_t inc = (tmr_increcnt[idx] += cycle) >> shift;
-
-                TM_CNT[idx] += inc;
-                tmr_increcnt[idx] -= inc << shift;
+                TM_COUNT[idx] = MemRead16(TM0_D + 0x4*idx);
+                MemWrite16(TM0_D + 0x4*idx, count+1);
+                TM_COUNT[idx] = MemRead16(TM0_D + 0x4*idx);
             }
         }
+        else{
+            uint8_t shift = shift_lut[TM_CNT[idx] & 3];
+            uint64_t inc = (tmr_increcnt[idx] += cycle) >> shift;
 
-        if(overflow = (count > 0xffff)){
-            count = reload + count - 0x10000;
+            TM_CNT[idx] += inc;
+            tmr_increcnt[idx] -= inc << shift;
+        }
+
+        if(overflow = (TM_COUNT[idx] > 0xffff)){
+            TM_COUNT[idx] = reload + TM_COUNT[idx] - 0x10000;
             uint16_t sound_cnt = MemRead16(SOUNDCNT_H);
             if(((sound_cnt >> 10) & 1) == idx){
                 //DMA sound A FIFO
